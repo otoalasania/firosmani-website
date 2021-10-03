d45a1e2cbad64a9ac330141da4f08db9c3eb4eb9
otoalasania <otar.alasania@gmail.com>
2021-10-03 14:50:00 +0400

    new project

---
+++ app.js
@@ -0,0 +1,22 @@
+mediumZoom('.zoom', {
+    margin: 50
+})
+
+window.addEventListener("scroll", function (event) {
+    let scroll = this.scrollY;
+    const pirveli = document.getElementById("heart");
+    const hat = document.getElementById("hat");
+    const rightArm = document.getElementById("right-arm");
+    if (scroll > 100) {
+        pirveli.style.top = "80%";
+        pirveli.style.transition = "1.5s";
+        hat.style.top = "-15%";
+        hat.style.transition = "1.5s";
+        rightArm.style.top = "40%";
+        rightArm.style.transition = "1.5s";
+    } else if (scroll < 100) {
+        pirveli.style.top = "";
+        hat.style.top = "";
+        rightArm.style.top = "";
+    }
+})
