2bf13c25334bc15ae746bcee8947c2e6535087ff
otoalasania <otar.alasania@gmail.com>
2021-10-06 02:48:07 +0400

    clean up code

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
 mediumZoom('.zoom', {
     margin: 50
-})
-
-window.addEventListener("scroll", function (event) {
-    let scroll = this.scrollY;
-    const heart = document.getElementById("heart");
-    const hat = document.getElementById("hat");
-    const rightArm = document.getElementById("right-arm");
-    if (scroll > 100) {
-        heart.style.top = "80%";
-        heart.style.transition = "1.5s";
-        hat.style.top = "-10%";
-        hat.style.transition = "1.5s";
-        rightArm.style.top = "50%";
-        rightArm.style.transition = "1.5s";
-    } else if (scroll < 150) {
-        heart.style.top = "";
-        hat.style.top = "";
-        rightArm.style.top = "";
-    }
 });
 
 const navBar = document.getElementById("navBar");
